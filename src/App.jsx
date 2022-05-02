163ec00cae04717945254643d53d8f6ee0f55ed9
eKerney <Eric.Kerney@airspacelink.com>
2022-05-02 16:17:46 -0400

    text/jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import { useRef } from 'react';
 import { Cartesian3, Color } from 'cesium'
 import { Viewer, Entity } from 'resium'
 
 function App() {
+
+  const ref = useRef(null);
+
   return (
-    <Viewer full>
+    <Viewer full
+        ref={ref}
+        baseLayerPicker={true}
+        infoBox={true}
+        homeButton={false}
+        timeline={false}
+        scene3DOnly={true}
+        shadows={true}
+        fullscreenButton={false}
+        animation={false}  
+    >
       <Entity
         name="Tokyo"
         position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
         point={{ pixelSize: 10, color: Color.WHITE }}
-        description="hoge"
+        description="Tokyo"
       />
     </Viewer>
   )
