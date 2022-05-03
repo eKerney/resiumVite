032e1a03209124c129d27bc1314f00cc97a813b8
eKerney <Eric.Kerney@airspacelink.com>
2022-05-03 13:46:02 -0400

    stuff

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
+// import { defineConfig } from 'vite';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+// import cesium from 'vite-plugin-cesium';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/resiumVite/',
+//   plugins: [reactRefresh(), cesium()],
+// })
+
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import cesium from 'vite-plugin-cesium';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/resiumVite/',
-  plugins: [reactRefresh(), cesium()],
+  plugins: [react(), cesium()]
 })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import cesium from 'vite-plugin-cesium';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), cesium()]
-// })
